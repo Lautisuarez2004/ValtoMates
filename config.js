884c2d0b3e04346487bd34938be9291c9ecca4f2
Lautisuarez2004 <suarez.ing.facultad@gmail.com>
2026-08-22 14:46:41 -0300

    Connect Valto dev to Supabase backend

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.VALTO_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://ptkzmshcfarerufnrrzq.supabase.co",
+  supabaseAnonKey: "sb_publishable__rY7nJoJv0Nd6QCDFafIog_t2dfQ6sl",
   whatsappNumber: "",
   instagramUrl: "",
   email: "valtomateslp@gmail.com"
