99a7028b95dbcf2d58d609687a817ddf7132281a
Lautisuarez2004 <suarez.ing.facultad@gmail.com>
2026-08-22 14:51:16 -0300

    Use JWT anon key for direct Supabase REST calls

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.VALTO_CONFIG = {
   supabaseUrl: "https://ptkzmshcfarerufnrrzq.supabase.co",
-  supabaseAnonKey: "sb_publishable__rY7nJoJv0Nd6QCDFafIog_t2dfQ6sl",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0a3ptc2hjZmFyZXJ1Zm5ycnpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3NTU1NzAsImV4cCI6MjEwMjMzMTU3MH0.eKgLehO8wnV_Jt0vY-R0LQj51RNA7Te39BMHE2iHqxU",
   whatsappNumber: "",
   instagramUrl: "",
   email: "valtomateslp@gmail.com"
