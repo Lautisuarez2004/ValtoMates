44eda5e8bfba8baed5e6b920f21d089511f209da
Lautisuarez2004 <suarez.ing.facultad@gmail.com>
2026-08-25 10:40:55 -0300

    Add social links configuration

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ window.VALTO_CONFIG = {
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0a3ptc2hjZmFyZXJ1Zm5ycnpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3NTU1NzAsImV4cCI6MjEwMjMzMTU3MH0.eKgLehO8wnV_Jt0vY-R0LQj51RNA7Te39BMHE2iHqxU",
   whatsappNumber: "",
   instagramUrl: "",
+  tiktokUrl: "",
   email: "valtomateslp@gmail.com"
 };
 
